512fd3cc835582268e9e6fe108252eb170f41dee
jasperhowald <jazdrum3r@yahoo.com>
2014-08-27 17:20:11 -0600

    A bit o' Teleop stuff.
    Pulled from
    http://wpilib.screenstepslive.com/s/3120/m/7885/l/79459-the-hello-world-
    of-frc-robot-programming.
    
    Check this to see if it works, and integrate it with the other stuff.
    Yes, I know we don’t use tank drive, but think of that as a temporary
    placeholder drive orientation.

--- src/org/usfirst/frc4068/code/Teleop.java
+++ src/org/usfirst/frc4068/code/Teleop.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc4068.code;
 
 public class Teleop {
-
+	//This code was pulled, slightly modified, off a tutorial found at http://wpilib.screenstepslive.com/s/3120/m/7885/l/79459-the-hello-world-of-frc-robot-programming
+	chassis.setSafetyEnabled(true);
+	while (isOperatorControl() && inEnabled()) {
+		chassis.tankDrive(leftStick, rightStick);
+		Timer.delay(0.01);
+	}
 }
